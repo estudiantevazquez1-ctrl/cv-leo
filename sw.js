9fb8787496ab399c8d05f7db34bb638bd6dd377f
estudiantevazquez1-ctrl <estudiantevazquez1@gmail.com>
2026-07-17 17:40:06 -0300

    Crear sw.js

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'stm-electric-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) return response;
+        return fetch(event.request).catch(() => {
+          return new Response('Sin conexion. La app funciona offline.');
+        });
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
